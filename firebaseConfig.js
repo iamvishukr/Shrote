9c597d322e2987f68d49d11bbb185e8a620c62c6
[Vishal Kumar] <[iamvishukr@gmail.com]>
2024-12-30 23:13:13 +0530

    updated firebase settings

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,16 +7,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBz0rLEje8UzaMdUIwRUVys3Co8oTRQbBg",
-  authDomain: "shrote-58b7f.firebaseapp.com",
-  projectId: "shrote-58b7f",
-  storageBucket: "shrote-58b7f.firebasestorage.app",
-  messagingSenderId: "308704464307",
-  appId: "1:308704464307:web:1eb2c66655cb3708974fc6",
-  measurementId: "G-1GLEY2EMS2"
+  apiKey: "AIzaSyCvnfAi5-lI0Kv8lny2TElJA7pVXcC7UHk",
+  authDomain: "shrote-7992d.firebaseapp.com",
+  projectId: "shrote-7992d",
+  storageBucket: "shrote-7992d.firebasestorage.app",
+  messagingSenderId: "756698956212",
+  appId: "1:756698956212:web:781a9ec81803c9fe0b32e1",
+  measurementId: "G-V2RP79N0H4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
 export default app;
