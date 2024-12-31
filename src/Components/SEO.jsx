d63b7d9bda4859bbb79914fb1043a9a99695b2cc
shrote <kgourav038@gmail.com>
2024-12-31 13:04:20 +0530

    added opengraph tag

--- src/Components/SEO.jsx
+++ src/Components/SEO.jsx
@@ -10,14 +10,22 @@ const SEO = ({ title, description, keywords, image }) => {
       <title>{`${title} | ${siteTitle}`}</title>
       <meta name="description" content={description || siteDescription} />
       <meta name="keywords" content={keywords} />
-      <meta property="og:title" content={`${title} | ${siteTitle}`} />
-      <meta property="og:description" content={description || siteDescription} />
-      <meta property="og:image" content={image || '/assets/img/og-image.jpg'} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary_large_image" />
+
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary_large_image" /> 
+      <meta name="twitter:site" content="@Shrote_services" /> 
+      <meta name="twitter:creator" content="@Shrote_services" /> 
       <meta name="twitter:title" content={`${title} | ${siteTitle}`} />
       <meta name="twitter:description" content={description || siteDescription} />
-      <meta name="twitter:image" content={image || '/assets/img/og-image.jpg'} />
+      <meta name="twitter:image" content={image || '../assets/og-image.png'} />
+
+      {/* Facebook Card */}
+      <meta property="og:title" content={`${title} | ${siteTitle}`} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.shrote.com" /> 
+      <meta property="og:image" content={image || '../assets/og-image.png'} />
+      <meta property="og:description" content={description || siteDescription} />
+      <meta property="og:site_name" content={siteTitle} /> 
     </Helmet>
   );
 };
