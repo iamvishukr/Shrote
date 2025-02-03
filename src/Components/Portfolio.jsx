111367ea55fcf973b47dcca4c1d1690025ca963b
Alok345 <96630014+Alok345@users.noreply.github.com>
2025-02-03 23:29:55 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -51,7 +51,7 @@ const Portfolio = forwardRef((_, ref) => {
               >
                 <img
                   src={item.image} // Use the imported image directly here
-                  className="object-cover w-full h-64 transition-transform duration-300 p-4 group-hover:scale-110"
+                  className="object-cover w-full h-64 transition-transform duration-300 group-hover:scale-110"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent group-hover:opacity-100 transition-opacity duration-300">
                   <div className="absolute bottom-0 left-0 right-0 p-4">
